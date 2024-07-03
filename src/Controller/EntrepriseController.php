4359dcabd9c0bfef9682482650b221a403a4d551
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-03 16:36:20 +0200

    updt

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -11,17 +11,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntrepriseController extends AbstractController
 {
-    #[Route('/entreprise', name: 'app_entreprise')]
+    #[Route('/entreprise', name: 'app_entreprise')] // le name va servir dans les "path" qu'on va mettre dans nos href sur les pages html pour appeler les différentes méthodes que l'on souhaite
     // public function index(EntityManagerInterface $entityManager): Response
     public function index(EntrepriseRepository $entrepriseRepository): Response //clic droit sur EntrepriseRepository et 'import class'
     {
         // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
         // $entreprises = $entrepriseRepository->findAll();
-        // SELECT * FROM entreprise WHERE ville = 'Mulhouse' ORDER BY raisonSociale ASC
-        $entreprises = $entrepriseRepository->findBy(["ville" => "Mulhouse"], ["raisonSociale" => "ASC"]); 
+        // SELECT * FROM entreprise ORDER BY raisonSociale ASC
+        $entreprises = $entrepriseRepository->findBy([], ["raisonSociale" => "ASC"]); 
         return $this->render('entreprise/index.html.twig', [  // méthode qui fait le lien entre le controlleur et la vue, Note : on peut également renvoyer une vue sans transmettre de 'data'
             'entreprises' => $entreprises
         ]);
     }
 
+    #[Route('/entreprise/{id}', name: 'show_entreprise')]
+    public function show(Entreprise $entreprise): Response // ici avec cette syntaxe, symfony est capable d'aller récupérer l'id de l'objet entreprise
+    {
+        return $this->render('entreprise/show.html.twig', [  // méthode qui fait le lien entre le controlleur et la vue, Note : on peut également renvoyer une vue sans transmettre de 'data'
+            'entreprise' => $entreprise
+        ]);
+    }
 }
+
