a766053e48bc44d5638bca6dc353403a240414dc
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-04 13:30:58 +0200

    updt

--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -4,29 +4,33 @@ namespace App\Form;
 
 use App\Entity\Employe;
 use App\Entity\Entreprise;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EmployeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('dateNaissance', null, [
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('dateNaissance', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('dateEmbauche', null, [
+            ->add('dateEmbauche', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('ville')
-            ->add('entreprise', EntityType::class, [
-                'class' => Entreprise::class,
-                'choice_label' => 'id',
+            ->add('ville', TextType::class)
+            ->add('entreprise', EntityType::class, [ // on choisit le type EntityType::class, on va avoir besoin de spécifier des choses dans un tableau d'arguments
+                'class' => Entreprise::class, // on inqique qu' Entreprise::class est notre classe (qu'il faut penser à importer) NOTE ceci peut être le cas car on a mis un tostring qui renvoie la raisonSociale
+                'choice_label' => 'raisonSociale', // on choisit sur quel propriété d'Entreprise on effectue la sélection. 
             ])
+            ->add('valider', SubmitType::class)
         ;
     }
 
--- src/Form/EntrepriseType.php
+++ src/Form/EntrepriseType.php
@@ -6,19 +6,23 @@ use App\Entity\Entreprise;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EntrepriseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('raisonSociale')
-            ->add('dateCreation', null, [
+            ->add('raisonSociale', TextType::class) // il faut bien penser à importer les classes utilisées, ici TextType ('import class', choisir celui dans component)
+            ->add('dateCreation', DateType::class, [ // il faut également importer la classe DateType
                 'widget' => 'single_text',
             ])
-            ->add('adresse')
-            ->add('cp')
-            ->add('ville')
+            ->add('adresse', TextType::class)
+            ->add('cp', TextType::class)
+            ->add('ville', TextType::class)
+            ->add('valider', SubmitType::class) // on ajoute le bouton SubmitType, et on importe également la classe SubmitType, les classes utilisables dans les forms sont accessibles dans la doc symfony. 
         ;
     }
 
