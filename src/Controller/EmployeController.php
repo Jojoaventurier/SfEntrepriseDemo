d19785fee4d04d6e789aed34627e12d73fee0c6a
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-07-04 12:59:52 +0200

    updt

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +22,18 @@ class EmployeController extends AbstractController
         $employes = $employeRepository->findBy([], ['nom' => 'ASC']);
         return $this->render('employe/index.html.twig', [  // méthode qui fait le lien entre le controlleur et la vue, Note : on peut également renvoyer une vue sans transmettre de 'data'
             'employes' => $employes
+        ]);
+    }
+
+    #[Route('/employe/new', name: 'new_employe')]
+    public function new(Request $request): Response // il faut penser à importer la classe request (depuis httpfoundation)
+    {
+        $employe = new employe();
+        
+        $form = $this->createForm(EmployeType::class, $employe); // il faut bien penser à importer la classe EmployeType
+        
+        return $this->render('employe/new.html.twig', [
+            'formAddEmploye' => $form,
         ]);
     }
 
